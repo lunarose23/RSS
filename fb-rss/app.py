5172b02a4dbf2998d631c3c2bee15d0715ccaa69
lunarose23 <lunarose23@gmail.com>
2026-02-23 20:01:49 -0500

    Update app.py

--- fb-rss/app.py
+++ fb-rss/app.py
@@ -245,7 +245,7 @@ def serve_image(filename):
 
 @app.route("/debug/<filename>")
 def debug_image(filename):
-    return send_from_directory("/data", filename)
+    return send_from_directory("/tmp", filename)
 
 @app.route("/debug/html/<filename>")
 def debug_html(filename):
